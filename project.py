580f85771af691b0ec68456d6c5f9fcdbd189a1a
Sami <119399293+samiahmed19@users.noreply.github.com>
2023-05-07 23:09:04 +0530

    Delete project.py

--- project.py
+++
@@ -1,9 +0,0 @@
-#8.Update the manufacture details of all the red-colored toys which are purchased by the “MyKids” store.
-import mysql.connector
-mydb=mysql.connector.connect(host='localhost',user='root',password='samiA12,',database='Inventory_management')
-cur=mydb.cursor()
-t=" upadte manufacture color='red' where store='mykidstore'  "
-cur.execute(t)
-display=cur.fetchall()
-for x in display:
-    print(x)
